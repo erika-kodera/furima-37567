d62ca00c50c8c75a1d58a7728b01a7722c3bcc6b
erika-kodera <98879043+erika-kodera@users.noreply.github.com>
2022-03-07 19:48:50 +0900

    新規登録のバリデーツの追加
    registrations/new.html.erbは
    エラーメッセージのポップアップで試行錯誤してます。

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,12 @@ class User < ApplicationRecord
   has_many :purchase_records
   
   validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :last_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :last_name_kana, presence: true
+
+  validates :family_name, presence: true, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+  validates :last_name, presence: true, with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
+
+  validates :family_name_kana, presence: true, with: /\A[ァ-ヴー]+\z/u
+  validates :last_name_kana, presence: true, with: /\A[ァ-ヴー]+\z/u
+
   validates :birth_day, presence: true
-  end
+end
