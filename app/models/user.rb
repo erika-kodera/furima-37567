8b4543296c547cfee040f9152ae2e75b26eb237a
erika-kodera <98879043+erika-kodera@users.noreply.github.com>
2022-03-08 17:43:39 +0900

    新規登録のモデルテスト
    ・gemのインストール
    ・モデル単体テスト（新規登録）のテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :items 
   has_many :purchase_records
   
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: { maximum: 40 }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
   validates :family_name, presence: true
   validates :last_name, presence: true
