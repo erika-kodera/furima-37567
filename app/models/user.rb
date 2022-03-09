d41eab07c63320d2d05ab45904145bb444835219
erika-kodera <98879043+erika-kodera@users.noreply.github.com>
2022-03-09 12:55:01 +0900

    passwordに半角英数字混合のバリデーションを追加。
    passwordのテストの結果を追加。

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,9 @@ class User < ApplicationRecord
     validates :last_name_kana
   end
   validates :birth_day, presence: true
+  
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, presence: true, length: { minimum: 6 }, 
+  format: {with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}, allow_nil: true
+
 end
