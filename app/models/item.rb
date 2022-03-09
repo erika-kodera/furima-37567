eb7dc3f62856bbc602e554f219dce888d3fddf06
erika-kodera <98879043+erika-kodera@users.noreply.github.com>
2022-03-09 22:46:46 +0900

    ActiveHashの導入
    ActiveHashの導入と各データの記述。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
-class Furima < ApplicationRecord
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
   has_one_attached :image
+
+  validates :item_name, 
+  validates :info, :text, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_time_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,
 end
