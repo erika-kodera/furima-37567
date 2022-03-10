f42521f2a438d97e21c37972e06a95a6fbd7068a
erika-kodera <98879043+erika-kodera@users.noreply.github.com>
2022-03-10 09:58:38 +0900

    バリデーションの編集、他

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,18 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :shipping_time
+
   has_one_attached :image
 
-  validates :item_name, 
-  validates :info, :text, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :shipping_time_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :price,
+  validates :item_name,          presence: true
+  validates :info, :text,        presence: true
+  validates :category_id,        presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id,       presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_charge_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id,      presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_time_id,   presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price,              presence: true
 end
