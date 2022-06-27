e5fbcbe7add5b7ba5b15f6987a516e08c139e2f8
erika-kodera <98879043+erika-kodera@users.noreply.github.com>
2022-06-27 17:09:40 +0900

    古いプレビューを削除する記述を削除
    画像が差し替えになってしまうので、古いプレビューを削除する記述を一旦削除。

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
     // 追加されたfile_fieldにchangeイベントをセット
     newFileField.addEventListener("change", changedFileField);
-    
+
     // 生成したfile_fieldを表示
     const fileFieldsArea = document.querySelector('.click-upload');
     fileFieldsArea.appendChild(newFileField);
@@ -49,11 +49,6 @@ const changedFileField = (e) => {
     // data-index(何番目を操作しているか)を取得
     const dataIndex = e.target.getAttribute('data-index');
 
-    // 古いプレビューが存在する場合は削除
-    const alreadyPreview = document.querySelector('.preview');
-    if (alreadyPreview) {
-      alreadyPreview.remove();
-    };
     const file = e.target.files[0];
     const blob = window.URL.createObjectURL(file);
 
